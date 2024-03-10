cb9986d1855073725b6d203581a4ca27b36234f8
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2024-03-10 19:59:21 +0330

    completed step 1

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,22 +1,18 @@
 import axios from "axios";
 
+// signing in with phone number (step 1)
 export const create_otp = async (phoneNumber) => {
   let data = JSON.stringify({ phone_number: phoneNumber });
-  console.log(data);
-  axios
-    .request({
-      method: "post",
-      maxBodyLength: Infinity,
-      url: "https://stage-api.sanaap.co/api/v2/app/DEY/agent/verification/signup/create_otp/",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: data,
-    })
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch((error) => {
-      console.log(error.response.data.error_details.fa_details);
-    });
+
+  const response = await axios.request({
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://stage-api.sanaap.co/api/v2/app/DEY/agent/verification/signup/create_otp/",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  });
+
+  return JSON.stringify(response.data);
 };
--- src/components/PhoneLogin.jsx
+++ src/components/PhoneLogin.jsx
@@ -1,46 +1,60 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { create_otp } from "../api/axios";
 
 const PhoneLogin = () => {
   const phoneRef = useRef("");
+  const [errorText, setErrorText] = useState("No error");
+  const [showError, setShowError] = useState("invisible");
 
-  const handleClick = () => {
-    create_otp(phoneRef.current);
+  const handleClick = async () => {
+    try {
+      const res = await create_otp(phoneRef.current);
+      console.log("hey there", res);
+    } catch (error) {
+      console.log(error.response.data.error_details.fa_details);
+      setErrorText(error.response.data.error_details.fa_details);
+      setShowError("visible");
+    }
   };
 
   return (
-    <>
-      <form>
-        <label
-          htmlFor="phone"
-          className="w-[100%] flex flex-col justify-between items-center"
-        >
-          <span className="mb-6 text-xl">
-            شماره موبایل خود را وارد نمایید.
-            <p className="text-gray-500 text-base">
-              کد تایید برای شما ارسال خواهد شد.
-            </p>
-          </span>
+    <form className="bg-white">
+      <label
+        htmlFor="phone"
+        className="w-[100%] flex flex-col justify-between items-center transition-all "
+      >
+        <span className="mb-6 text-xl">
+          شماره موبایل خود را وارد نمایید.
+          <p className="text-gray-500 text-base">
+            کد تایید برای شما ارسال خواهد شد.
+          </p>
+        </span>
 
-          <input
-            name="phone"
-            type="tel"
-            placeholder="شماره همراه"
-            onChange={(e) => (phoneRef.current = e.target.value)}
-            required
-            className="border-2 rounded-2xl border-gray-300 py-3 px-4 w-[80%] mb-14 text-right"
-          />
-        </label>
-
-        <button
-          type="button"
-          onClick={() => handleClick()}
-          className="bg-teal-600 text-white w-[80%] mb-5 py-2 rounded-lg hover:bg-teal-700 focus:bg-teal-700"
+        <input
+          name="phone"
+          type="tel"
+          placeholder="شماره همراه"
+          onChange={(e) => (phoneRef.current = e.target.value)}
+          required
+          className="border-2 rounded-2xl border-gray-300 py-3 px-4 w-[80%] mb-2 text-right"
+        />
+        <div
+          className={`
+        mb-14 w-[80%] px-4 text-red-600  text-sm ml-auto text-right mx-7 ${showError} 
+        `}
         >
-          ادامه
-        </button>
-      </form>
-    </>
+          {errorText}
+        </div>
+      </label>
+
+      <button
+        type="button"
+        onClick={() => handleClick()}
+        className="bg-teal-600 text-white w-[80%] mb-5 py-2 rounded-lg hover:bg-teal-700 focus:bg-teal-700"
+      >
+        ادامه
+      </button>
+    </form>
   );
 };
 
